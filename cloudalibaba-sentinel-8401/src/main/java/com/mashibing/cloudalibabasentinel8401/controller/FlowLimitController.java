3f7089fd85624c870e24052e4858c627a75f3aae
朱一一 <zhuyiyi1990@outlook.com>
2022-04-04 17:52:32 +0800

    章节26：Sentinel流控规则 课时26 Sentinel流控规则

--- cloudalibaba-sentinel-8401/src/main/java/com/mashibing/cloudalibabasentinel8401/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-8401/src/main/java/com/mashibing/cloudalibabasentinel8401/controller/FlowLimitController.java
@@ -3,11 +3,19 @@ package com.mashibing.cloudalibabasentinel8401.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA() {
+        //暂停0.8秒
+        try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "-----testA";
     }
 
