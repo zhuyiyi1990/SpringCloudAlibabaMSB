71b913d9d199477badca28a8b70109c7997645e5
朱一一 <zhuyiyi1990@outlook.com>
2022-04-07 15:56:59 +0800

    章节40：SentinelResource的fallback属性 课时40 SentinelResource的fallback属性

--- cloudalibaba-consumer-8084/src/main/java/com/mashibing/cloudalibabaconsumer8084/controller/DemoController.java
+++ cloudalibaba-consumer-8084/src/main/java/com/mashibing/cloudalibabaconsumer8084/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.mashibing.cloudalibabaconsumer8084.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.mashibing.cloudalibabacommons.entity.JsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +23,28 @@ public class DemoController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/fallback/{id}")
+    //添加SentinelResource注解的fallback属性，同时设置方法来解决Java异常
+    @SentinelResource(value = "fallback", fallback = "fallbackHandler", blockHandler = "blockHandler", exceptionsToIgnore = {NullPointerException.class})
     public JsonResult<String> fallback(@PathVariable Long id) {
-        //通过Ribbon发起远程访问，访问9003/9004
-        JsonResult<String> result = restTemplate.getForObject(SERVICE_URL + "/info/" + id, JsonResult.class);
+        if (id <= 3) {
+            //通过Ribbon发起远程访问，访问9003/9004
+            JsonResult<String> result = restTemplate.getForObject(SERVICE_URL + "/info/" + id, JsonResult.class);
+            return result;
+        } else {
+            throw new NullPointerException("没有对应的数据记录");
+        }
+    }
+
+    //保证方法签名基本保持一致，但是要添加异常类型参数
+    //处理Java异常
+    public JsonResult<String> fallbackHandler(Long id, Throwable e) {
+        JsonResult<String> result = new JsonResult<>(444, "NullPointerException异常");
+        return result;
+    }
+
+    //处理Sentinel限流
+    public JsonResult<String> blockHandler(Long id, BlockException e) {
+        JsonResult<String> result = new JsonResult<>(445, "BlockException限流");
         return result;
     }
 
