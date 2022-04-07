e27813f0a70c7667d20e2813ad25a74cd57c831f
朱一一 <zhuyiyi1990@outlook.com>
2022-04-07 20:41:23 +0800

    章节43：OpenFeign日志打印 课时43 OpenFeign日志打印

--- cloudalibaba-openfeign-consumer-8888/src/main/java/com/mashibing/cloudalibabaopenfeignconsumer8888/CloudalibabaOpenfeignConsumer8888Application.java
+++ cloudalibaba-openfeign-consumer-8888/src/main/java/com/mashibing/cloudalibabaopenfeignconsumer8888/CloudalibabaOpenfeignConsumer8888Application.java
@@ -1,9 +1,11 @@
 package com.mashibing.cloudalibabaopenfeignconsumer8888;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +14,12 @@ public class CloudalibabaOpenfeignConsumer8888Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudalibabaOpenfeignConsumer8888Application.class, args);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        //开启详细日志
+        return Logger.Level.FULL;
     }
 
 }
