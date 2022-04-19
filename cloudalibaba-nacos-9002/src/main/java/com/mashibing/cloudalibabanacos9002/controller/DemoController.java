04059ebd2a0ab834b7ace197733d020c3e1dfcea
朱一一 <zhuyiyi1990@outlook.com>
2022-04-19 16:25:48 +0800

    章节62：GateWay实现负载均衡 课时62 GateWay实现负载均衡

--- cloudalibaba-nacos-9002/src/main/java/com/mashibing/cloudalibabanacos9002/controller/DemoController.java
+++ cloudalibaba-nacos-9002/src/main/java/com/mashibing/cloudalibabanacos9002/controller/DemoController.java
@@ -2,17 +2,24 @@ package com.mashibing.cloudalibabanacos9002.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/msb")//路由路径
 public class DemoController {
 
     @Value("${server.port}")
     private String serverPort;
 
-    @GetMapping(value = "/mashibing")
+    @GetMapping(value = "/get")
     public String getServerPort() {
-        return "Hello Nacos Discovery" + serverPort;
+        return "库存-1：" + serverPort;
+    }
+
+    @GetMapping(value = "/custom")
+    public String customTest() {
+        return "网关配置测试~~costom";
     }
 
 }
