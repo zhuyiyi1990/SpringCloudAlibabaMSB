73e1840cd1bc6c688f7efb3d7a672909414045dc
朱一一 <zhuyiyi1990@outlook.com>
2022-04-20 09:53:34 +0800

    章节63：GateWay断言Predicate 课时63 GateWay断言Predicate

---
+++ cloudalibaba-gateway-9999/src/test/java/com/mashibing/cloudalibabagateway9999/TestDateTime.java
@@ -0,0 +1,12 @@
+package com.mashibing.cloudalibabagateway9999;
+
+import java.time.ZonedDateTime;
+
+public class TestDateTime {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();//默认时区
+        System.out.println(zbj);
+    }
+
+}
